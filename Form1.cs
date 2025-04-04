ba4f172f1afc709f7bb0a57a59a9c70618f23e2c
Lab2-29 <Lab2-29@LAB2-29>
2025-04-03 18:19:43 -0600

    Agregar archivos de proyecto.

---
+++ Form1.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace LOGIN
+{
+    public partial class FrmLOGGIN : Form
+    {
+        Usuario usuarios = new Usuario();
+        public FrmLOGGIN()
+        {
+            InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        private void btnIngresar_Click(object sender, EventArgs e)
+        {
+            string user = tbxusuario.Text;
+            string password = tbxcontraseña.Text;
+
+            var lista = usuarios.Obtenerusuario();
+            var validar = lista.FirstOrDefault(u => u.NombreUsuario == user && u.Contrasenia == password);
+
+            if (validar != null)
+            {
+                this.Hide();
+                Inicio inicio = new Inicio();
+                inicio.Show();
+            }
+            else
+            {
+
+                MessageBox.Show("Fallaste...");
+            }
+        }
+    }
+}
---
+++ Usuario.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LOGIN
+{
+    internal class Usuario
+    {
+
+        private static List<Usuario> usuarios = new List<Usuario>
+        {
+        new Usuario{NombreUsuario = "Admin", Contrasenia="1234"},
+        new Usuario{NombreUsuario = "Yeyito", Contrasenia="0000"},
+        new Usuario{NombreUsuario = "Imael", Contrasenia="1111"},
+
+};
+        public Usuario() { }
+
+        public List<Usuario> Obtenerusuario()
+        {
+            return usuarios;
+        }
+
+        public string NombreUsuario { get; set; }
+        public string Contrasenia { get; set; }
+    }
+}
